bf044fb01fbdf6593afcbb515b0ca6e619533deb
WaxyPine <jacqueslucas.m2101@gmail.com>
2026-01-17 14:23:20 +0100

    refactor: Suppression du code mort/dupliquer

--- src/script/eraser.js
+++
@@ -1,7 +0,0 @@
-const canvas = paint;
-const brush = canvas.getContext("2d");
-brush.fillStyle = "#fff";
-brush.fillRect(0, 0, innerwidth, innerheight);
-brush.lineCap = "round";
-brush.lineWidth = 80;
-brush.globalCompositeOperation = "destination-out";
